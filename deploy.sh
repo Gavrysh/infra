03ad74c974bdfc984a3402da84adbc403108f2d5
Sergij Gavrysh <gases.ua@gmail.com>
2020-04-06 14:15:54 +0300

    Add scripts for unit 06

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~
+
+# Copy application from GitHub (use https)
+git clone https://github.com/Artemmkin/reddit.git
+
+# change dir and setup dependence application
+cd reddit && bundle install
+
+# run application server in project folder
+puma -d
+
+# check running application server and port
+ps aux | grep puma
+
