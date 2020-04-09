9066b440e1df1c419a42ebe3e22072600bc9ccae
Sergij Gavrysh <gases.ua@gmail.com>
2020-04-09 21:03:39 +0300

    add provisioner 'remote-exec'

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,11 @@ resource "google_compute_instance" "app" {
   provisioner "file" {
     source = "files/puma.service"
     destination = "/tmp/puma.service"
-}
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 
 }
 
