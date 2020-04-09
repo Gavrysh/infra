3a9430a539171cca20e59ada5ad40ac877b3c606
Sergij Gavrysh <gases.ua@gmail.com>
2020-04-09 20:49:24 +0300

    add tag to instance VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,8 @@ resource "google_compute_instance" "app" {
   machine_type = "g1-small"
   zone = "europe-west1-b"
 
+  tags = ["reddit-app"]
+
 # определение загрузочного диска
   boot_disk {
     initialize_params {
