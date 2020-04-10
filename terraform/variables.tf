02b510e1c174d30ef4e746364c1a96815db90794
Sergij Gavrysh <gases.ua@gmail.com>
2020-04-10 21:49:23 +0300

    separation of configuration files, create two VM

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,11 @@ variable private_key_path {
 variable disk_image {
   description = "Disk image"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+}
